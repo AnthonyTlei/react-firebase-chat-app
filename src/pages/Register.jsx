d45c2efd44f2c273c886044a44598f2d292af426
Anthony Tleiji <anthonytleiji@gmail.com>
2023-03-14 11:11:59 +0200

    Created UI for Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,24 @@
+import AddAvatar from "../assets/images/addAvatar.png"
+
 const Register = () => {
     return ( 
-        <div>
-            Register
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">Chat App</span>
+                <span className="title">Register</span>
+                <form>
+                    <input type="text" name="" id="" placeholder="display name"/>
+                    <input type="email" name="" id="" placeholder="email"/>
+                    <input type="password" name="" id="" placeholder="password"/>
+                    <input style={{display:"none"}} type="file" name="" id="file" />
+                    <label htmlFor="file">
+                        <img src={AddAvatar} alt="avatar" />
+                        <span>Add an avatar</span>
+                    </label>
+                    <button>Sign up</button>
+                </form>
+                <p>Already have an account? Login</p>
+            </div>
         </div>
      );
 }
