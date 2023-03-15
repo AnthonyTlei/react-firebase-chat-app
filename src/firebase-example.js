995fefb0b4081c59a714dedc571d17c7244ef3c8
Anthony Tleiji <anthonytleiji@gmail.com>
2023-03-15 14:56:18 +0200

    Added firebase Authentication

---
+++ src/firebase-example.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const analytics = getAnalytics(app);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,49 @@
-import AddAvatar from "../assets/images/addAvatar.png"
+import AddAvatar from "../assets/images/addAvatar.png";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useState } from "react";
 
 const Register = () => {
-    return ( 
-        <div className="formContainer">
-            <div className="formWrapper">
-                <span className="logo">Chat App</span>
-                <span className="title">Register</span>
-                <form>
-                    <input type="text" name="" id="" placeholder="display name"/>
-                    <input type="email" name="" id="" placeholder="email"/>
-                    <input type="password" name="" id="" placeholder="password"/>
-                    <input style={{display:"none"}} type="file" name="" id="file" />
-                    <label htmlFor="file">
-                        <img src={AddAvatar} alt="avatar" />
-                        <span>Add an avatar</span>
-                    </label>
-                    <button>Sign up</button>
-                </form>
-                <p>Already have an account? Login</p>
-            </div>
-        </div>
-     );
-}
- 
+  const [error, setError] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat App</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="" id="" placeholder="display name" />
+          <input type="email" name="" id="" placeholder="email" />
+          <input type="password" name="" id="" placeholder="password" />
+          <input style={{ display: "none" }} type="file" name="" id="file" />
+          <label htmlFor="file">
+            <img src={AddAvatar} alt="avatar" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+          {error && <span>Something went wrong...</span>}
+        </form>
+        <p>Already have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
 export default Register;
