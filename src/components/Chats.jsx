fc070d66debda507d3d54b0383dae646b66919da
Anthony Tleiji <anthonytleiji@gmail.com>
2023-03-18 18:02:35 +0200

    Implemented Chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,34 +1,34 @@
+import { useState, useEffect, useContext } from "react";
+import { onSnapshot, doc } from "firebase/firestore";
+import { AuthContext } from "../context/AuthContext";
+import { db } from "../firebase";
 const Chats = () => {
-    return (
-        <div className="chats">
-
-            <div className="userChat">
-                <img src="https://images3.alphacoders.com/128/1286461.jpg" alt="chat-user-avatar" />
-                <div className="userChatInfo">
-                    <span>Aki</span>
-                    <p>Hello</p>
-                </div>
+  const { currentUser } = useContext(AuthContext);
+  const [chats, setChats] = useState([]);
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+  return (
+    <div className="chats">
+      {Object.entries(chats)?.map((chat) => (
+        <div className="userChat" key={chat[0]}>
+            <img src={chat[1].userInfo.photoURL} alt="" />
+            <div className="userChatInfo">
+                <span>{chat[1].userInfo.displayName}</span>
+                <p>{chat[1].userInfo.lastMessage?.text}</p>
             </div>
-
-            <div className="userChat">
-                <img src="https://images3.alphacoders.com/128/1286461.jpg" alt="chat-user-avatar" />
-                <div className="userChatInfo">
-                    <span>Aki</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="userChat">
-                <img src="https://images3.alphacoders.com/128/1286461.jpg" alt="chat-user-avatar" />
-                <div className="userChatInfo">
-                    <span>Aki</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-
         </div>
-    );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default Chats;
