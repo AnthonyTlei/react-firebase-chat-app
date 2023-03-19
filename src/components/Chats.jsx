9a01bf43be4f87acfdf34bd21796288b332bc5f4
Anthony Tleiji <anthonytleiji@gmail.com>
2023-03-19 17:43:17 +0200

    Fixed image upload

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -25,7 +25,7 @@ const Chats = () => {
 
   return (
     <div className="chats">
-      {Object.entries(chats)
+      {chats && Object.entries(chats)
         ?.sort((a, b) => b[1].date - a[1].date)
         .map((chat) => (
           <div
@@ -33,7 +33,7 @@ const Chats = () => {
             key={chat[0]}
             onClick={() => handleSelect(chat[1].userInfo)}
           >
-            <img src={chat[1].userInfo.photoURL} alt="" />
+            <img src={chat[1].userInfo.photoURL} alt="user-photo" />
             <div className="userChatInfo">
               <span>{chat[1].userInfo.displayName}</span>
               <p>{chat[1].lastMessage?.text}</p>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -13,6 +13,7 @@ import {
 import { db, storage } from "../firebase";
 import { v4 as uuid } from "uuid";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { json } from "react-router-dom";
 
 const Input = () => {
   const [text, setText] = useState("");
@@ -24,9 +25,27 @@ const Input = () => {
       const storageRef = ref(storage, uuid());
       const uploadTask = uploadBytesResumable(storageRef, img);
       uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
         (error) => {
+          // Handle unsuccessful uploads
+          console.log("error: ");
+          console.log(error);
         },
         () => {
+          console.log("Finished upload");
           getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
             await updateDoc(doc(db, "chats", data.chatId), {
               messages: arrayUnion({
